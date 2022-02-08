e4feff8f62661e2710c226b884f907680dfbd548
Vee Satayamas <5ssgdxltv@relay.firefox.com>
2022-02-09 00:38:16 +0700

    Use zstd_read_line 0.2.0 API

--- zstd-read-line.asd
+++ zstd-read-line.asd
@@ -4,8 +4,10 @@
   :description "zstd-read-line is a zstd_read_line's wrapper for Common Lisp"
   :author "Vee Satayamas <5ssgdxltv@relay.firefox.com>"
   :license  "GPL-3.0"
-  :version "0.0.1"
+  :version "0.2.0"
   :serial t
   :depends-on (#:cffi)
   :components ((:file "package")
-               (:file "zstd-read-line")))
+               (:file "zstd-read-line"))
+  :build-operation "program-op"
+  :build-pathname "build.bash")
--- zstd-read-line.lisp
+++ zstd-read-line.lisp
@@ -2,10 +2,6 @@
 
 (in-package #:zstd-read-line)
 
-(print (type-of (namestring
-	 (asdf:system-relative-pathname :zstd-read-line
-					"zstd_read_line/target/release/zstd_read_line"))))
-
 (define-foreign-library zstd_read_line
   (:win32 #.(namestring
 	   (asdf:system-relative-pathname :zstd-read-line
@@ -18,13 +14,18 @@
 (use-foreign-library zstd_read_line)
 
 (defcfun ("zstd_line_read_new" create-reader*) :pointer (zstd_archive_path :string))
-(defcfun ("zstd_line_read" zstd-read-line*) :pointer (reader :pointer))
+(defcfun ("zstd_line_read" zstd-read-line*)
+    :int (reader :pointer) (line-ptr :pointer) (line-len-ptr :pointer))
 (defcfun ("zstd_line_read_delete_line" delete-line) :void (line :pointer))
 (defcfun ("zstd_line_read_delete" close-reader) :void (reader :pointer))
 
 (define-condition unable-to-open-archive (error)
   ((archive-path :initarg archive-path
 		 :reader archive-path)))
+
+(define-condition unable-to-read-archive (error)
+  ((archive-path :initarg archive-handler
+		 :reader archive-handler)))
 
 (defun create-reader (zstd-archive-path)
   (let ((reader (create-reader* zstd-archive-path)))
@@ -33,8 +34,13 @@
 	reader)))
 
 (defun zstd-read-line (reader)
-  (let ((buf (zstd-read-line* reader)))
-    (unless (null-pointer-p buf)
-      (let ((line (foreign-string-to-lisp buf)))
-	(delete-line buf)
-	line))))
+  (with-foreign-objects ((line-len-ptr :pointer) (line-ptr :pointer))
+    (let ((status-code (zstd-read-line* reader line-ptr line-len-ptr)))
+      (when (/= status-code 0)
+	(error 'unable-to-read-archive :archive-handler reader))
+      (let* ((line-len (mem-ref line-len-ptr :int))
+	     (line (mem-ref line-ptr :pointer)))
+	(unless (null-pointer-p line)
+	  (let ((lisp-line (foreign-string-to-lisp line :count line-len)))
+	    (delete-line line)
+	    lisp-line))))))
